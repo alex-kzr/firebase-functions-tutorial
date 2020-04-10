249267635b3a19deee4361a1728835d2d85a24c9
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-10 12:42:03 +1000

    Add Vue instance

--- public/js/requests.js
+++ public/js/requests.js
@@ -1,27 +1,37 @@
-const ref = firebase.firestore().collection('requests');
-let requests = [];
-ref.onSnapshot(snapshot => {
-    
-    snapshot.docChanges().forEach(change => {
-        if(change.type === 'added'){
-            requests.push({...change.doc.data(), id: change.doc.id});
-        }else if(change.type === 'removed'){
-            requests = requests.filter(doc => doc.id != change.doc.id);
-        }
-        
-        console.log(requests);
-        let html = ``;
-        requests.forEach(request => {
-            html += `
-                <li>
-                    <span class="text">${request.text}</span>
-                    <div>
-                        <span class="votes">${request.upvotes}</span>
-                        <i class="material-icons upvote">arrow_upward</i>
-                    </div>
-                </li>
-            `;
+var app = new Vue({
+    el: '#app',
+    data: {
+        requests: []
+    },
+    mounted(){
+        const ref = firebase.firestore().collection('requests');
+        let requests = [];
+        ref.onSnapshot(snapshot => {
+            
+            snapshot.docChanges().forEach(change => {
+                if(change.type === 'added'){
+                    requests.push({...change.doc.data(), id: change.doc.id});
+                }else if(change.type === 'removed'){
+                    requests = requests.filter(doc => doc.id != change.doc.id);
+                }
+                this.requests = requests;       
+            });
         });
-        document.querySelector('ul').innerHTML = html;
-    });
-});
+    }
+  })
+
+
+
+//   let html = ``;
+//   requests.forEach(request => {
+//       html += `
+//           <li>
+//               <span class="text">${request.text}</span>
+//               <div>
+//                   <span class="votes">${request.upvotes}</span>
+//                   <i class="material-icons upvote">arrow_upward</i>
+//               </div>
+//           </li>
+//       `;
+//   });
+//   document.querySelector('ul').innerHTML = html;
