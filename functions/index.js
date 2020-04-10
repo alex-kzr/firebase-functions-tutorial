1c5ad024911a0b3110967be75b8c219461984c7e
Alex-SK <aleksey.kozyura@gmail.com>
2020-04-10 21:39:10 +1000

    Add cloud function for tracking activity

--- functions/index.js
+++ functions/index.js
@@ -77,3 +77,28 @@ exports.upvote = functions.https.onCall(async (data,context) => {
         upvotes: admin.firestore.FieldValue.increment(1)
     });        
 });
+
+// firestore trigger for tracking activity
+exports.logActivities = functions.firestore.document('/{collection}/{id}')
+    .onCreate((snap, context) => {
+        console.log(snap.data());
+        
+        const collection = context.params.collection;
+        const id = context.params.id;
+
+        const activities = admin.firestore().collection('activities');
+
+        if(collection === 'requests'){
+            return activities.add({ 
+                text: 'a new tutorial request was added', 
+                id
+            });
+        }
+        if(collection === 'users'){
+            return activities.add({ 
+                text: 'a new user signed up',
+                id
+            });
+        }
+        return null;
+    });
